3f2aa4f639b9bb58abf17167dcba8831dd1ee45b
codenchunks <codenchunks@gmail.com>
2024-10-14 10:37:18 +0530

    Add files via upload

---
+++ DSA with Python/Pseudocode.py
@@ -0,0 +1,17 @@
+'''
+
+Begin
+
+    Input FirstNumber
+
+    Input SecondNumber
+
+    Add FirstNumber and SecondNumber and print the result
+
+    SUM = FirstNumber + SecondNumber
+
+    print SUM
+
+End
+
+'''
